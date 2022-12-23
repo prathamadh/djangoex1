8f457af31e3650b3f6897c344dcf6acc96a2f9a4
pratham <apratham489@gmail.com>
2022-12-23 17:49:49 +0545

    initial commit

---
+++ home/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Contact(models.Model):
+    name=models.CharField(max_length=120)
+    email=models.CharField(max_length=120)
+    phone=models.CharField(max_length=120)
+    date=models.DateField()
+    desc=models.TextField()
+    def __str__(self):
+        return self.name
